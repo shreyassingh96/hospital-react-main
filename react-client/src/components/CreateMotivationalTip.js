b10b469c9cd5e657ab4da70479aefde612918592
Mayuran <90732301+MayuranSri@users.noreply.github.com>
2023-04-21 20:32:13 -0400

    UI Change
    Update UI

--- react-client/src/components/CreateMotivationalTip.js
+++ react-client/src/components/CreateMotivationalTip.js
@@ -8,76 +8,76 @@ import { withRouter, useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb, faMessage } from '@fortawesome/free-solid-svg-icons';
 
-// GraphQL mutation to create a motivational tip
 export const Create_Tip = gql`
-  mutation($message: String!) {
-    createMotivationalTip(message: $message) {
-      _id
-    }
-  }
+mutation
+(
+  $message: String!,
+  ) 
+        {
+            createMotivationalTip
+        (
+            message: $message
+          )
+          {
+            _id
+        }
+      }
 `;
 
 function CreateTip(props) {
-  // Reference to the "message" input field
+
   let message;
 
-  // Hook to access the history object of the React Router
   const history = useHistory();
-
-  // Mutation hook to create a motivational tip
   const [createMotivationalTip, { data, loading, error }] = useMutation(Create_Tip);
 
-  // Handler for "Cancel" button click
   const onCancel = e => {
     e.preventDefault();
     history.push('/nurse');
   }
 
-  // Show a loading message while the mutation is in progress
   if (loading) return 'Submitting...';
-
-  // Show an error message if the mutation fails
   if (error) return `Submission error! ${error.message}`;
 
-  // Render the component
   return (
-    <div>
-      <h2><center> Motivational Tip</center></h2>
+    <div className='motive'>
+      <header className="tranback">
+        <center><h2>Motivational Tip</h2></center>
+      </header>
+     
+        <Form className='form' onSubmit={e => {
+          createMotivationalTip({
+            variables: {
+              message: message.value
+            }
+          });
 
-      <Form className='form' onSubmit={e => {
-        // Call the mutation to create a motivational tip
-        createMotivationalTip({
-          variables: {
-            message: message.value
-          }
-        });
+          message.value = '';
 
-        // Clear the input field
-        message.value = '';
 
-        // Show a success message
-        alert("Message sent!");
+          alert("Message sent!");
+          history.push('/nurse');
 
-        // Navigate to the nurse page
-        history.push('/nurse');
-      }}>
-        <Form.Group>
-          <center><Form.Label> Tip of the Day <FontAwesomeIcon icon={faLightbulb} size={'1x'} color='orange' /></Form.Label></center><br />
-          <Form.Control type="text" name="message" id="message" placeholder="Enter message" ref={node => { message = node; }} /><br />
-        </Form.Group>
+        }} >
 
-        <div className='button'>
-          <Button className='buttonSave' variant="primary" type="submit">
-            Send
-          </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className='buttonCancel' variant="danger" type="submit" onClick={onCancel}>
-            Cancel
-          </Button>
-        </div>
-      </Form>
+          <Form.Group>
+            <center><Form.Label> Tip of the Day <FontAwesomeIcon icon={faLightbulb} size={'1x'} color='orange' /></Form.Label></center><br />
+            <Form.Control type="text" name="message" id="message" placeholder="Enter message" ref={node => { message = node; }} /><br />
+          </Form.Group>
+
+          <div className='button'>
+            <Button className='buttonSave' variant="primary" type="submit">
+              Send
+            </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button className='buttonCancel' variant="danger" type="submit" onClick={onCancel}>
+              Cancel
+            </Button>
+          </div>
+        </Form>
+   
     </div>
   );
+
 }
 
-// Export the component wrapped in the withRouter higher-order component
 export default withRouter(CreateTip);
